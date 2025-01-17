12bb2baedb6ba0a499f17bed5288bdb2ddcd4e79
Michael Fleurimond <Michaelfleurimond301@gmail.com>
2025-01-17 10:13:58 -0500

    assume I put a good message here

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import usersRoutes from `./routes/users.js`
 const app = express();
 const PORT = 5000;
 
 //// middlewear
 app.use(bodyParser.json());
-
+app.use(`/users`,usersRoutes);
 /// get routes 
 app.get("/",(req,res)=>{
     res.send("Hi I am the the slash route!! LOVE ME")
